ff84ea66e0448f9ed559017ca96f762844cc6e75
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-04-08 12:16:04 -0400

    Basic NodeJS: import,export

--- Basic-Node-JS/app.js
+++ Basic-Node-JS/app.js
@@ -1,8 +1,12 @@
 //Don't use var, It's recommended use let and const
 
 //console.log("PROCESS", process);
+//const helpers = require('./helpers');
 
-const helpers = require('./helpers');
+const { sum, minus } = require('./helpers');
 
-const total = helpers.sum(10, 50);
-console.log("Total: ", total);
+const totalSum = sum(10, 50);
+const totalMinus = minus(60, 20);
+
+console.log("Total Sum: ", totalSum);
+console.log("Total Minus: ", totalMinus);
--- Basic-Node-JS/helpers.js
+++ Basic-Node-JS/helpers.js
@@ -1,7 +1,16 @@
-function sum(a, b) {
+/*function sum(a, b) {
     return a + b;
-}
+}*/
+
+/*module.exports = {
+    sum
+};*/
+
+const sum = (a, b) => a + b;
+
+const minus = (a, b) => a - b;
 
 module.exports = {
-    sum
+    sum,
+    minus
 };
