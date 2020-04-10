1cf6e2dfaa80b951842c43af225618b7bfd3fd95
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-04-10 14:12:25 -0400

    Intermediate-Node-API: Express and Mongoose

--- Intermediate-Node-API/src/app.js
+++ Intermediate-Node-API/src/app.js
@@ -1,3 +1,24 @@
-const { info } = require('./modules/log');
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-info("Helloe");
+const { info, error } = require('./modules/log');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// ENV
+dotenv.config();
+
+// BD
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => info('mongoDB, Atlas. Connected'))
+    .catch((err) => error(err));
+
+// Middleware
+app.use(morgan('dev'));
+
+app.listen(port, () => {
+    info(`A NodeJS API is listining on port: ${port}`);
+});
