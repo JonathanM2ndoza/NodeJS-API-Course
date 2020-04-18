372ecf68c5ff43b3ea7c90d901e84bff45e9f717
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-04-18 13:49:24 -0400

    Advanced Node API: Improved promises, Code cleanup

--- Advanced-Node-API/src/config/database/mongo.ts
+++ Advanced-Node-API/src/config/database/mongo.ts
@@ -17,4 +17,25 @@ export default (app: Application, mongoURI: string, port: number): void => {
       });
     })
     .catch((err) => error(err));
+
+  mongoose.connection.on('connected', function () {
+    console.log('Mongoose default connection is open');
+  });
+
+  mongoose.connection.on('error', function (err) {
+    console.error('Failed to connect to DB ' + mongoURI + ' on startup ', err);
+  });
+
+  mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection is disconnected');
+  });
+
+  process.on('SIGINT', function () {
+    mongoose.connection.close(function () {
+      console.log(
+        'Mongoose default connection is disconnected due to application termination'
+      );
+      process.exit(0);
+    });
+  });
 };
