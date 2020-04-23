f636836bb76b98a5df782d62ca475dafff6d03c8
Jonathan Mendoza <jonathan.m2ndoza@gmail.com>
2020-04-23 14:13:46 -0400

    Unit Test: BDD / TDD (mocha,chai)

---
+++ Advanced-Node-API/test/products/users.ts
@@ -0,0 +1,54 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import { ConnectionMongoDB } from '../../src/config/database/mongo';
+
+const conn = new ConnectionMongoDB();
+const server = 'http://localhost:3000';
+
+//Assertion Style
+chai.should();
+chai.use(chaiHttp);
+
+describe('GET /api/v1/users', () => {
+  before(async () => {
+    try {
+      await conn.connect();
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  after(async () => {
+    try {
+      conn.close();
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  it('GET /api/v1/login', (done) => {
+    const body = {
+      email: 'jonathan.m2ndoza1@gmail.com',
+      password: '123456jm-',
+    };
+
+    chai
+      .request(server)
+      .post('/api/v1/login')
+      .send(body)
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.body.should.be.a('object');
+        response.body.should.have.property('status').eq('OK');
+        response.body.should.have
+          .property('message')
+          .to.be.an('object')
+          .to.have.property('token');
+        response.body.should.have
+          .property('message')
+          .to.be.an('object')
+          .to.have.property('tokenExpire');
+        done();
+      });
+  });
+});
